5e4ab80911c2dbf98ca88ed1a40c0cd1c548731a
ajramos27 <aj.ramos2794@gmail.com>
2020-09-21 19:13:48 -0700

    First commit

---
+++ TravelRecordApp/TravelRecordApp/NewTravelPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using TravelRecordApp.Model;
+using SQLite;
+
+namespace TravelRecordApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewTravelPage : ContentPage
+    {
+        public NewTravelPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Post post = new Post()
+            {
+                Experience = experienceEntry.Text
+            };
+
+            SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation);
+            conn.CreateTable<Post>();
+            int rows = conn.Insert(post);
+            conn.Close();
+
+            if(rows > 0)
+            {
+                DisplayAlert("Sucess", "Experience inserted","Ok");
+            } else
+            {
+                DisplayAlert("Sucess", "Experience failed", "Ok");
+            }
+        }
+    }
+}
