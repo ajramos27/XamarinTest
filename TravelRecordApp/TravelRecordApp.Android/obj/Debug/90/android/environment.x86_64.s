4d199e9971e6fdf49439894de291398683f35693
ajramos27 <aj.ramos2794@gmail.com>
2020-09-24 22:54:46 -0700

    Maps and Geolocation

--- TravelRecordApp/TravelRecordApp.Android/obj/Debug/90/android/environment.x86_64.s
+++ TravelRecordApp/TravelRecordApp.Android/obj/Debug/90/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"681ded17-686d-4f51-b01c-8d420fa50d19"
+	.asciz	"d31ebb63-23e7-4f06-9149-61b3b6d00c5e"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
--- TravelRecordApp/TravelRecordApp/MapPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/MapPage.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +14,91 @@ namespace TravelRecordApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MapPage : ContentPage
     {
+
+        private bool hasLocationPermissions = false;
         public MapPage()
         {
             InitializeComponent();
+            GetPermissions();
+        }
+
+        private async void GetPermissions()
+        {
+            try
+            {
+                var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+                if (status != PermissionStatus.Granted)
+                {
+                    status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+                }
+
+                if (status == PermissionStatus.Granted)
+                {
+                    locationsMap.IsShowingUser = true;
+                    hasLocationPermissions = true;
+                    GetLocation();
+
+                } else
+                {
+                    await DisplayAlert("Location denied", "Please allow access to location", "Ok");
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "Ok");
+            }
+            
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (hasLocationPermissions)
+            {
+                var locator = CrossGeolocator.Current;
+                locator.PositionChanged += Locator_PositionChanged;
+                await locator.StartListeningAsync(TimeSpan.Zero, 100);
+
+            }
+            
+            GetLocation();
+            
+           
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            //Stop and unsuscribed
+            CrossGeolocator.Current.StopListeningAsync();
+            CrossGeolocator.Current.PositionChanged -= Locator_PositionChanged;
+        }
+
+        private void Locator_PositionChanged(object sender, Plugin.Geolocator.Abstractions.PositionEventArgs e)
+        {
+            MoveMap(e.Position);
+        }
+
+        private async void GetLocation()
+        {
+            if (hasLocationPermissions)
+            {
+                var locator = CrossGeolocator.Current;
+                //var request = new GeolocationRequest(GeolocationAccuracy.Medium);
+                //var location = await Geolocation.GetLocationAsync(request);
+
+                var position = await locator.GetPositionAsync();
+                MoveMap(position);
+                
+            }
+           
+        }
+        
+        private void MoveMap(Position position)
+        {
+            var center = new Xamarin.Forms.Maps.Position(position.Latitude, position.Longitude);
+            locationsMap.MoveToRegion(new Xamarin.Forms.Maps.MapSpan(center, 1, 1));
         }
     }
 }
